5ad9c375b573dc7e3ab32ff4e943e9929c14d7d3
zzangin77 <jungin@gimjeong-in-ui-MacBookAir.local>
2020-08-09 23:49:33 +0900

    Initial commit

---
+++ week03.py
@@ -0,0 +1,34 @@
+from pymongo import MongoClient
+
+client = MongoClient('localhost', 27017)
+db = client.DBspatra
+
+import requests
+from bs4 import BeautifulSoup
+
+# URL을 읽어서 HTML를 받아오고,
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+data = requests.get('https://www.genie.co.kr/chart/top200?ditc=D&rtm=N&ymd=20200713', headers=headers)
+
+# HTML을 BeautifulSoup이라는 라이브러리를 활용해 검색하기 용이한 상태로 만듦
+soup = BeautifulSoup(data.text, 'html.parser')
+
+trs = soup.select('#body-content > div.newest-list > div > table > tbody > tr')
+
+
+
+for tr in trs:
+    rank = tr.select_one('td.number').text[0:2].strip()
+    title = tr.select_one('td.info > a.title.ellipsis').text.strip()
+    artist = tr.select_one('td.info > a.artist.ellipsis').text
+    print(rank,title,artist)
+
+    doc = {
+    'rank' : rank,
+    'title' : title,
+    'artist' : artist,
+
+}
+
+    db.songs.insert_one(doc)
